c7566c6ff788aa30f5f3cc207b732a7811dfbb29
Pradeep <pradeepprady005@gmail.com>
2025-08-23 19:16:59 +0530

    refactor(Header): remove commented phone number section
    The phone number section was commented out as it's no longer needed in the header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,12 +54,12 @@ const Header = () => {
             {/* Contact Info & CTA */}
             <div className="hidden lg:flex items-center space-x-6">
               {/* Phone Number */}
-              <div className="flex items-center space-x-2">
+              {/* <div className="flex items-center space-x-2">
                 <svg className="w-4 h-4 text-black" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
                 <span className="text-black font-medium">+91-9876543210</span>
-              </div>
+              </div> */}
 
               {/* CTA Buttons */}
               <div className="flex gap-3">
